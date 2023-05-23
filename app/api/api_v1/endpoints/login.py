d8bb274fd42775f0b7e426db2d0a46136ab70f20
Vaibhav Kesharwani <vaibhav.vk2128@gmail.com>
2023-05-23 11:53:20 +0530

    Fixed an endpoint

--- app/api/api_v1/endpoints/login.py
+++ app/api/api_v1/endpoints/login.py
@@ -36,7 +36,7 @@ def login_access_token(
     access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
     return {
         "access_token": security.create_access_token(
-            user.id, expires_delta=access_token_expires
+            subject=user.id, name=f'{user.first_name} {user.last_name}',  expires_delta=access_token_expires
         ),
         "token_type": "bearer",
     }
